4a782edb4bc4e195e076e31b0ffe51d7f30352ba
Matt Kwiecien <mkwiecien@bigtime.net>
2020-10-03 09:00:36 -0700

    Completed UI for first step of tutorial.

---
+++ GitCommit2/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  GitCommit2
+//
+//  Created by Matt Kwiecien on 10/3/20.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var detailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
