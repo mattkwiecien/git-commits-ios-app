536bbd56aaf4cc2fb2e059ef46da9d10245046cd
Matt Kwiecien <mkwiecien@bigtime.net>
2020-10-04 08:06:18 -0700

    Project building and running.  Data being pulled and showing on the screen.

--- GitCommit2/ViewController.swift
+++ GitCommit2/ViewController.swift
@@ -12,14 +12,10 @@ import SwiftyJSON
 class ViewController: UITableViewController {
 
     var container: NSPersistentContainer!
-    
+    var commits = [Commit]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let commit = Commit()
-        commit.message = "Woo"
-        commit.url = "http://www.example.com"
-        commit.date = Date()
         
         //Create the container
         container = NSPersistentContainer(name: "GitCommit")
@@ -29,8 +25,25 @@ class ViewController: UITableViewController {
                 print("Unresolved error \(error)")
             }
         }
+        
         performSelector(inBackground: #selector(fetchCommits), with: nil)
-
+        loadSavedData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
+        let commit = commits[indexPath.row]
+        cell.textLabel!.text = commit.message
+        cell.detailTextLabel!.text = commit.date.description
+        return cell
     }
     
     @objc func fetchCommits() {
@@ -45,6 +58,7 @@ class ViewController: UITableViewController {
                     self.configure(commit: commit, usingJSON: jsonCommit)
                 }
                 self.saveContext();
+                self.loadSavedData()
             }
         }
     }
@@ -65,6 +79,20 @@ class ViewController: UITableViewController {
             } catch {
                 print("An error occurred while saving: \(error)")
             }
+        }
+    }
+    
+    func loadSavedData(){
+        let request = Commit.createFetchRequest()
+        let sort = NSSortDescriptor(key: "date", ascending: false)
+        request.sortDescriptors = [sort]
+        
+        do {
+            commits = try container.viewContext.fetch(request)
+            print("Got \(commits.count) commits.")
+            tableView.reloadData()
+        } catch{
+            print("Fetch failed.")
         }
     }
 
